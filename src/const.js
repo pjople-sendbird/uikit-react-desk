4b7bc3753f5a680be3e45985efbefe59e63703e6
warodri-sendbird <walter.rodriguez@sendbird.com>
2022-04-01 11:24:46 +0100

    First commit

---
+++ src/const.js
@@ -0,0 +1,13 @@
+// put your own APP_ID here
+// get your app_id -> https://dashboard.sendbird.com/auth/signin
+export const APP_ID = "6A6CF887-E6F6-4763-84B6-BEFD2410EE42";
+// set your own USER_ID and NICKNAME
+export const USER_ID = "sendbird_desk_agent_id_03f1dafa-cf49-41aa-993f-bf8cae7710e9";
+export const NICKNAME = "Desk Agent";
+export const ACCESS_TOKEN = "2c5a0f5ed9999837cf10c50356ac6321e5340d8f";
+
+export default {
+  APP_ID,
+  USER_ID,
+  NICKNAME
+};
